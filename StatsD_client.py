00091786ea4492af3d986429597090b1585f1c90
Brian Hartford <brian.hartford@datadoghq.com>
2022-10-16 22:50:35 -0400

    updated typo

--- StatsD_client.py
+++ StatsD_client.py
@@ -11,9 +11,9 @@ initialize(**options)
 
 
 while(1):
-  statsd.distribution('gunicorn.request.duration.avg', random.randint(0, 100), tags=["reason:statsd_test"])
-  statsd.increment('gunicorn.test_count.increment', random.randint(0, 100), tags=["reason:statsd_test"])
-  statsd.gauge("gunicorn.test_gauge", random.randint(0, 75), tags=["reason:statsd_test"])
+  statsd.distribution('custom.request.duration.avg', random.randint(0, 100), tags=["reason:statsd_test"])
+  statsd.increment('custom.test_count.increment', random.randint(0, 100), tags=["reason:statsd_test"])
+  statsd.gauge(`custom.test_gauge`, random.randint(0, 75), tags=["reason:statsd_test"])
   #statsd.decrement('gunicorn.test_count.decrement', tags=["environment:statsd_test"])
   time.sleep(2)
 
